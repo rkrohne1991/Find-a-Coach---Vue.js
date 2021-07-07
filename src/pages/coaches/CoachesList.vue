9225a06e3c46ef6aea7443b23fc0b45d26000bf4
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-07-07 07:52:54 +0100

    Adding authentication - locking/protecting backend resources, login & signup, preparing vuex, loading spinner & error handling, attaching token to outgoing requests, auto login, auto logout

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -10,7 +10,8 @@
             <base-card>
                 <div class="controls">
                     <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-                    <base-button v-if="!isCoach && !isLoading" link to="'/register">Register as Coach</base-button>
+                    <base-button link to="/auth?redirect=register" v-if="!isLoggedIn">Login to Register as Coach</base-button>
+                    <base-button v-if="isLoggedIn && !isCoach && !isLoading" link to="'/register">Register as Coach</base-button>
                 </div>
                 <div v-if="isLoading">
                     <base-spinner></base-spinner>
@@ -52,6 +53,9 @@ export default {
         };
     },
     computed: {
+        isLoggedIn() {
+            return this.$store.getters.isAuthenticated;
+        },
         filteredCoaches() {
             const coaches = this.$store.getters['coaches/coaches'];
             return coaches.filter(coach => {
--- src/router.js
+++ src/router.js
@@ -7,7 +7,11 @@ import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 
+import UserAuth from './pages/auth/UserAuth.vue';
+
 import NotFound from './pages/NotFound.vue';
+
+import store from './store/index.js';
 
 const router = createRouter({ 
     history: createWebHistory(),
@@ -22,10 +26,21 @@ const router = createRouter({
                 { path: 'contact', component: ContactCoach } // /coaches/c/1/contact
             ],
         },
-        { path: '/register', component: CoachRegistration },
-        { path: '/requests', component: RequestsReceived },
+        { path: '/register', component: CoachRegistration, meta: { requiresAuth: true } },
+        { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+        { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
         { path: '/:notFound(.*)', component: NotFound },
     ],
+});
+
+router.beforeEach((to, _, next) => {
+    if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+        next('/auth');
+    } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+        next('/coaches')
+    } else {
+        next();
+    }
 });
 
 export default router;
